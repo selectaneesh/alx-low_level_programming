224e87302d80af0e364b9d31891b974fa4170b40
selecta_neesh <dennymunene9@gmail.com>
2023-03-21 17:41:04 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_alphabet - Make the aiphabet
+*
+* Return: void
+*/
+
+void print_alphabet(void)
+{
+
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+
+_putchar(c);
+
+}
+
+_putchar('\n');
+
+}
+
